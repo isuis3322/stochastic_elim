de7e990fb103045a81f7d63a6835b6b4d7784b16
Kuan-Hua Tu <isuis3322@gmail.com>
2019-07-01 02:08:48 +0800

    add basic script

---
+++ TestWhole.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Describtion: Automatic build and test
+
+Root_Dir=$(pwd)
+
+# move to build file
+test -e "$Root_Dir/src/abc" || (echo "ABC bot found"; exit 1)
+cd $Root_Dir/src/abc
+
+# compile
+echo "# Compiling..."
+make
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+# run self unit test
+echo "# Unit testing..."
+TEMP_EXE=$Root_Dir/src/abc/abc
+$TEMP_EXE -q "utest"
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+# run each testcases
+echo "# Benchmark testing..."
+TEMP_EXE=$Root_Dir/src/abc/abc
+
+CASES_DIR=$Root_Dir/testcase/case
+RESULT_DIR=$Root_Dir/testcase/result
+test -e "$Root_Dir/testcase" || mkdir "$Root_Dir/testcase"
+test -e "$RESULT_DIR" || mkdir "$RESULT_DIR"
+
+if [ -e $CASES_DIR ]; then
+  CASE_LIST=$(ls $CASES_DIR)
+  for file in $CASE_LIST; do
+    ($TEMP_EXE -q "ssat_elim $CASES_DIR/$file") > $RESULT_DIR/$file.log
+	cat $RESULT_DIR/$file.log
+    # TODO: use verify program
+  done
+fi
+
+# 
+echo "All testing are passed: OK"
+
+
